abf4136029cd3b3f3dede66448d27e54b66b3a8c
3510324447 <3510324447@PC-1177>
2023-08-14 08:26:07 +0330

    Add project files.

---
+++ AspNetCoreAndTypeScript/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCoreAndTypeScript.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AspNetCoreAndTypeScript/wwwroot/scripts/app.js
@@ -0,0 +1,11 @@
+function sayHello() {
+    const compiler = document.getElementById("compiler")
+        .value;
+    const framework = document.getElementById("framework")
+        .value;
+    return `Hello from ${compiler} and ${framework}!`;
+}
+function sayTest() {
+    alert("test");
+}
+//# sourceMappingURL=app.js.map
